8189c8fad1c11133d4258e83653b63b17c00c6c8
Ares Chen <arechen@microsoft.com>
2018-04-21 00:13:26 +0800

    Console application template

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
+---
+//国际版
+// var ctx =new AuthenticationContext("https://login.microsoftonline.com/common/oauth2");
+// var deviceCode = ctx.AcquireDeviceCodeAsync("https://graph.microsoft.com","cfc3a225-ac57-45c9-aacd-969551f4825f").Result;
+// System.Console.WriteLine($"请打开浏览器，访问https://aka.ms/devicelogin,并以 {deviceCode.UserCode} 登陆");
+// var token = ctx.AcquireTokenByDeviceCodeAsync(deviceCode).Result.AccessToken;
+// request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",token);
+
+var ctx =new AuthenticationContext("https://login.chinacloudapi.cn/common/oauth2");
+var deviceCode = ctx.AcquireDeviceCodeAsync("https://microsoftgraph.chinacloudapi.cn","d430823c-5613-4b1a-8d39-78f29c549f3e").Result;
+System.Console.WriteLine($"请打开浏览器，访问 https://aka.ms/gallatindevicelogin,并以 {deviceCode.UserCode} 登陆");
+var token = ctx.AcquireTokenByDeviceCodeAsync(deviceCode).Result.AccessToken;
+Console.WriteLine(token);
+request.Headers.Authorization = new AuthenticationHeaderValue("Bearer",token);
+
+//reply url msalcfc3a225-ac57-45c9-aacd-969551f4825f://auth
+
+
+dotnet nuget push .\chenxizhang.dotnetcore.msgraph.console.CSharp.1.0.0.nupkg -s https://www.nuget.org/api/v2/package
+---
 # dotnetcore-office365dev-templates
---
+++ dotnetcore-graph-console/content/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace content
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 
+            var authority="";
+            var resource="";
+            var deviceloginaddr="";
+
+            #endregion
+
+            //#if(instance=="gallatin")
+            authority ="https://login.chinacloudapi.cn/common/oauth2";
+            resource ="https://microsoftgraph.chinacloudapi.cn";
+            deviceloginaddr="https://aka.ms/gallatindevicelogin";
+            
+            //#else
+            authority ="https://login.microsoftonline.com/common/oauth2";
+            resource="https://graph.microsoft.com";
+            deviceloginaddr="https://aka.ms/devicelogin";     
+       
+            //#endif
+
+            var token = "";
+
+            var client = new GraphServiceClient(new DelegateAuthenticationProvider(async (request) =>
+            {
+                if (string.IsNullOrEmpty(token))
+                {
+                    var ctx = new AuthenticationContext(authority: authority);
+                    var deviceCode = ctx.AcquireDeviceCodeAsync(resource, "{{clientId}}").Result;
+                    Console.WriteLine($"请打开浏览器，访问{deviceloginaddr},并以 {deviceCode.UserCode} 登陆");
+                    token = ctx.AcquireTokenByDeviceCodeAsync(deviceCode).Result.AccessToken;
+                }
+
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                await Task.FromResult(1);
+            }))
+            {
+                BaseUrl = $"{resource}/v1.0"
+            };
+
+            var user = client.Me.Request().GetAsync().Result;
+            Console.WriteLine($"当前登录用户为:{user.DisplayName}\r\n");
+
+            var messages = client.Me.Messages.Request().GetAsync().Result;
+            Console.WriteLine($"当前用户的收件箱最近十封邮件如下：");
+            foreach (var item in messages)
+            {
+                Console.WriteLine($"\t{item.Subject}");
+            }
+
+            var files = client.Me.Drive.Root.Children.Request().GetAsync().Result;
+            Console.WriteLine($"\r\n当前用户的个人网盘的文件信息如下：");
+            foreach (var item in files)
+            {
+                Console.WriteLine($"\t{item.Name}");
+            }
+
+        }
+    }
+}
